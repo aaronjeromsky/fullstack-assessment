1e5a54bb1e9980003f16b54f94a3fd8b3b655e27
Aaron Jeromsky <aaron.k.jeromsky@gmail.com>
2025-10-19 00:46:34 -0700

    fix: added missing image host names to `next.config.ts`

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'm.media-amazon.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'images-na.ssl-images-amazon.com',
+      }
     ],
   },
   outputFileTracingRoot: path.join(__dirname),
